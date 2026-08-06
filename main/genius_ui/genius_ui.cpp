f22ad387c00863f1c88410d80a211004f73ce1fd
Bapul <alupeasaja@gmail.com>
2026-08-06 08:58:58 +0700

    feat(ui): add initial Minji boot splash screen

--- main/genius_ui/genius_ui.cpp
+++ main/genius_ui/genius_ui.cpp
@@ -1,11 +1,27 @@
 #include "genius_ui.h"
 
+#include "boot_manager.h"
 #include "minji_face.h"
+
+
+namespace {
+
+void StartMinjiFace(
+    lv_obj_t* screen
+)
+{
+    MinjiFace::Init(screen);
+}
+
+}  // namespace
 
 
 void GeniusUI::Init(
     lv_obj_t* screen
 )
 {
-    MinjiFace::Init(screen);
+    BootManager::Start(
+        screen,
+        StartMinjiFace
+    );
 }
