9da74dd933096e597213a11ce603178d3fb00b55
Bapul <alupeasaja@gmail.com>
2026-08-06 09:11:39 +0700

    feat(face): add Face Engine core architecture

---
+++ main/genius_ui/face_engine/face_theme.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "lvgl.h"
+#include "face_state.h"
+
+class FaceTheme {
+public:
+    virtual ~FaceTheme() = default;
+
+    virtual bool Init(
+        lv_obj_t* screen
+    ) = 0;
+
+    virtual bool IsReady() const = 0;
+
+    virtual void ApplyState(
+        FaceState state
+    ) = 0;
+};
